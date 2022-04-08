5afe526acb383f34b2c0008b40a1fa6c1661c1cc
Shivkant <civkantkaushik@gmail.com>
2022-04-08 15:56:57 +0530

    First Commit: Created directory structure for express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// app variable will have all the functionalities of express, by calling express function and
+// assigned it to app variable
+const app = express();
+
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        // this functionality in below code is called interpolation
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${8000}`);
+})
